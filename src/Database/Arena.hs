0772b5f87655f14943954f14f9852fa17916c4b8
davean <davean@xkcd.com>
2015-10-10 18:34:27 -0400

    Leaving lunch.

--- src/Database/Arena.hs
+++ src/Database/Arena.hs
@@ -46,42 +46,69 @@ instance Serial JournalFrame where
       then return $ JF l h d
       else fail "Uh, this method shouldn't be in Monad"
 
-data OpenJournal b = OJ { ojHandle :: Handle, ojSummary :: b }
+data OpenJournal b = OJ { ojArenaID :: ArenaID, ojHandle :: Handle, ojSummary :: Option b }
 
 startArena :: (MonadIO m1, MonadIO m2, MonadIO m3, Serial a, Serial c, Semigroup b)
            => (a -> b) -> (b -> c) -> (b -> Bool) -> ArenaLocation
            -> m1 (IORef [(c, m2 [a])], a -> m3 ())
 startArena summerize finalize arenaFull diskLocation = do
-    currJournal <- internArenas >>= cleanJournal
+    currJournalM <- internArenas >>= cleanJournal >>= newMVar
     dr <- readAllData >>= newIORef
-    return (dr, undefined)
+    return (dr, addData dr currJournalM)
   where
-    addData :: IORef [(c, IO [a])] -> MVar FileHandle -> a -> IO ()
+    addData :: IORef [(c, IO [a])] -> MVar OpenJournal -> a -> IO ()
+    addData dsr ojM a = (flip modifyMVar_ ojM) $ \(OJ ai h s) -> do
+     BS.hPutStr h . runPutS $ a
+     let s' = s <> (pure . summerize $ a)
+     syncHandle h -- This COULD be moved down for a very minor perf boost.
+     case arenaFull . fromJust . getOption $ s' of
+       False -> return $ OJ ai h s'
+       True -> do
+         hClose h
+         internArenaFl ai
+         let ai' = ai + 1
+         h' <- openFile (arenaId2jfl diskLocation $ ai') WriteMode
+         return $ OJ ai' h' mempty
     syncHandle :: Handle -> IO ()
     syncHandle _ = do
       putStrLn "Wish we could!"
     readJournalFl :: FilePath -> IO [a]
     readJournalFl jf = do
       d <- BS.readFile jf
-      undefined
-    internArenaFl :: FilePath -> IO () --This should take an ID?
-    internArenaFl jf = do
-      je <- readJournalFl jf
-      withFile (dataFile diskLocation ) WriteMode $ \h -> do
+      error "Alec, this is your job."
+    internArenaFl :: ArenaID -> IO ()
+    internArenaFl ai = do
+      -- This has a problem if the journal was empty!
+      -- We never intern a journal untill we've moved on to another though,
+      -- and to move on to another, we have to have had a summary to check for nursery fullness.
+      je <- readJournalFl . arenaId2jfl diskLocation $ ai
+      withFile (dataFile diskLocation ai) WriteMode $ \h -> do
+        mapM_ (BS.hPutStr h . runPutS) as
+        syncHandle h
+      withFile (dataSummaryFile diskLocation ai) WriteMode $ \h -> do
+        BS.hPutStr h . runPutS . finalize . alec $ as
+        syncHandle h
+      removeFile . arenaId2jfl diskLocation $ ai
     internArenas :: IO ArenaID
     internArenas = do
       (js::[ArenaID]) <- mapMaybe readMay <$> (getDirectoryContents . journalDir $ diskLocation)
       let latest = maximum js
       let old = delete latest js
-      mapM_ (internArenaFl . arenaId2jfl diskLocation) old
+      mapM_ internArenaFl old
       return latest
     cleanJournal :: ArenaID -> OpenJournal b
     cleanJournal jf = do
       removeFile . tempJournal $ diskLocation
-      as <- readJournalFl . arenaId2jfl diskLocation $ jf
+      as <- readJournalFl . arenaId2jfl diskLocation $ ai
       withFile (tempJournal diskLocation) WriteMode $ \h -> do
         mapM_ (BS.hPutStr h . runPutS . mkJournal) as
         syncHandle h
+      renameFile (tempJournal diskLocation) (arenaId2jfl diskLocation ai)
+      removeFile ∘ tempJournal $ diskLocation
+      jh <- openFile (arenaId2jfl diskLocation ai) WriteMode
+      return (jh, alec as)
+    alec :: [a] -> b
+    alec = foldl1 (<>) . map summarize
     dfToCIOa :: ArenaID -> IO (c, IO [a])
     dfToCIOa a = do
       
