ec68525c273c4a0a46f2dce338ef0725d5f1a672
davean <davean@xkcd.com>
2015-10-10 21:53:34 -0400

    Compiles! Not done, but now Alec can do my job!

--- src/Database/Arena.hs
+++ src/Database/Arena.hs
@@ -5,21 +5,28 @@ module Database.Arena (
   ) where
 
 import Data.Word
+import Data.Maybe
+import Data.List
 import Data.Bytes.Serial
 import Data.Bytes.Get
 import Data.Bytes.Put
 import Data.Semigroup
 import Data.IORef
 import Control.Monad.Trans
+import Control.Concurrent.MVar
 import Data.Digest.CRC32
+import System.IO
 import System.FilePath
 import System.Directory
 import qualified Data.ByteString as BS
+import Safe
 
 newtype ArenaLocation = ArenaLocation FilePath
 
 journalDir :: ArenaLocation -> FilePath
 journalDir (ArenaLocation fp) = fp </> "journal"
+tempJournal :: ArenaLocation -> FilePath
+tempJournal al = journalDir al </> "temp"
 dataDir :: ArenaLocation -> FilePath
 dataDir (ArenaLocation fp) = fp </> "data"
 arenaId2jfl :: ArenaLocation -> ArenaID -> FilePath
@@ -34,7 +41,7 @@ type ArenaID = Word32
 data JournalFrame = JF { jLength :: Word32, jHash :: Word32, jData :: BS.ByteString }
 
 mkJournal :: BS.ByteString -> JournalFrame
-mkJournal d = JF (BS.length d) (crc32 d) d
+mkJournal d = JF (fromIntegral . BS.length $ d) (crc32 d) d
 
 instance Serial JournalFrame where
     serialize (JF l h d) = putWord32be l *> putWord32be h *> putByteString d
@@ -46,32 +53,46 @@ instance Serial JournalFrame where
       then return $ JF l h d
       else fail "Uh, this method shouldn't be in Monad"
 
-data OpenJournal b = OJ { ojArenaID :: ArenaID, ojHandle :: Handle, ojSummary :: Option b }
+data OpenJournal a b = OJ { ojArenaID :: ArenaID, ojHandle :: Handle, ojSummary :: Option b, ojVals :: [a] }
 
-startArena :: (MonadIO m1, MonadIO m2, MonadIO m3, Serial a, Serial c, Semigroup b)
+startArena :: forall m1 m2 m3 a b c
+           . (MonadIO m1, MonadIO m2, MonadIO m3, Serial a, Serial c, Semigroup b)
            => (a -> b) -> (b -> c) -> (b -> Bool) -> ArenaLocation
-           -> m1 (IORef [(c, m2 [a])], a -> m3 ())
-startArena summerize finalize arenaFull diskLocation = do
+           -> m1 (m2 [(c, m2 [a])], a -> m3 ())
+startArena summerize finalize arenaFull diskLocation = liftIO $ do
     currJournalM <- internArenas >>= cleanJournal >>= newMVar
     dr <- readAllData >>= newIORef
-    return (dr, addData dr currJournalM)
+    return (accessData dr currJournalM, addData dr currJournalM)
   where
-    addData :: IORef [(c, IO [a])] -> MVar OpenJournal -> a -> IO ()
-    addData dsr ojM a = (flip modifyMVar_ ojM) $ \(OJ ai h s) -> do
-     BS.hPutStr h . runPutS $ a
+    readAllData :: IO [(c, m2 [a])]
+    readAllData = undefined
+    accessData :: IORef [(c, m2 [a])] -> MVar (OpenJournal a b) -> m2 [(c, m2 [a])]
+    accessData dr cjM = liftIO $ do
+      withMVar cjM $ \(OJ _ _ s as) -> do
+        d <- readIORef dr
+        return $ (finalize . fromJust . getOption $ s, return . reverse $ as):d
+    addData :: IORef [(c, m2 [a])] -> MVar (OpenJournal a b) -> a -> m3 ()
+    addData dsr ojM a = liftIO . modifyMVar_ ojM $ \(OJ ai h s as) -> do
+     BS.hPutStr h . runPutS . serialize $ a
      let s' = s <> (pure . summerize $ a)
      syncHandle h -- This COULD be moved down for a very minor perf boost.
      case arenaFull . fromJust . getOption $ s' of
-       False -> return $ OJ ai h s'
+       False -> return $ OJ ai h s' (a:as)
        True -> do
          hClose h
          internArenaFl ai
+         atomicModifyIORef' dsr $ \ods -> 
+           ((finalize . fromJust . getOption $ s, readDataFl ai):ods, ())
          let ai' = ai + 1
          h' <- openFile (arenaId2jfl diskLocation $ ai') WriteMode
-         return $ OJ ai' h' mempty
+         return $ OJ ai' h' mempty mempty
     syncHandle :: Handle -> IO ()
     syncHandle _ = do
       putStrLn "Wish we could!"
+    readDataFl :: MonadIO m => ArenaID -> m [a]
+    readDataFl ai = liftIO $ do
+      d <- BS.readFile (dataFile diskLocation ai)
+      error "Alec, also do this."
     readJournalFl :: FilePath -> IO [a]
     readJournalFl jf = do
       d <- BS.readFile jf
@@ -81,12 +102,12 @@ startArena summerize finalize arenaFull diskLocation = do
       -- This has a problem if the journal was empty!
       -- We never intern a journal untill we've moved on to another though,
       -- and to move on to another, we have to have had a summary to check for nursery fullness.
-      je <- readJournalFl . arenaId2jfl diskLocation $ ai
+      as <- readJournalFl . arenaId2jfl diskLocation $ ai
       withFile (dataFile diskLocation ai) WriteMode $ \h -> do
-        mapM_ (BS.hPutStr h . runPutS) as
+        mapM_ (BS.hPutStr h . runPutS . serialize) as
         syncHandle h
       withFile (dataSummaryFile diskLocation ai) WriteMode $ \h -> do
-        BS.hPutStr h . runPutS . finalize . alec $ as
+        BS.hPutStr h . runPutS . serialize . finalize . alec $ as
         syncHandle h
       removeFile . arenaId2jfl diskLocation $ ai
     internArenas :: IO ArenaID
@@ -96,20 +117,19 @@ startArena summerize finalize arenaFull diskLocation = do
       let old = delete latest js
       mapM_ internArenaFl old
       return latest
-    cleanJournal :: ArenaID -> OpenJournal b
-    cleanJournal jf = do
+    cleanJournal :: ArenaID -> IO (OpenJournal a b)
+    cleanJournal ai = do
       removeFile . tempJournal $ diskLocation
       as <- readJournalFl . arenaId2jfl diskLocation $ ai
       withFile (tempJournal diskLocation) WriteMode $ \h -> do
-        mapM_ (BS.hPutStr h . runPutS . mkJournal) as
+        mapM_ (BS.hPutStr h . runPutS . serialize . mkJournal . runPutS . serialize) as
         syncHandle h
       renameFile (tempJournal diskLocation) (arenaId2jfl diskLocation ai)
-      removeFile ∘ tempJournal $ diskLocation
+      removeFile . tempJournal $ diskLocation
       jh <- openFile (arenaId2jfl diskLocation ai) WriteMode
-      return (jh, alec as)
+      return $ OJ ai jh (pure . alec $ as) as
     alec :: [a] -> b
-    alec = foldl1 (<>) . map summarize
+    alec = foldl1 (<>) . map summerize
     dfToCIOa :: ArenaID -> IO (c, IO [a])
     dfToCIOa a = do
-      
-
+      return (undefined, readDataFl a)
