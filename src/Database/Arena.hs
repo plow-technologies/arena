de627e863ce0c0e14388c2430acad94cf1265407
Alec <alec@deviant-logic.net>
2015-10-13 16:12:40 -0400

    removed non-monad-style code

--- src/Database/Arena.hs
+++ src/Database/Arena.hs
@@ -5,7 +5,6 @@
 module Database.Arena
     (
      ArenaLocation(..)
-    , startArena
 
     , ArenaT
     , Arena
@@ -44,16 +43,6 @@ import           System.IO
 newtype ArenaLocation = ArenaLocation { getArenaLocation :: FilePath }
   deriving (Eq, Ord, Read, Show, IsString)
 
-journalDir, dataDir, tempJournal :: ArenaLocation -> FilePath
-journalDir (ArenaLocation fp) = fp            </> "journal"
-dataDir    (ArenaLocation fp) = fp            </> "data"
-tempJournal al                = journalDir al </> "temp"
-
-journalFile, dataFile, dataSummaryFile :: ArenaLocation -> ArenaID -> FilePath
-journalFile     al i = journalDir al </> show i
-dataFile        al i = dataDir    al </> addExtension (show i) "data"
-dataSummaryFile al i = dataDir    al </> addExtension (show i) "header"
-
 type ArenaID     = Word32
 type JournalHash = Word32
 
@@ -85,91 +74,6 @@ readJournalFile l ai = do
   d <- BSL.readFile (journalFile l ai)
   return . map (head . rights . pure . runGetS deserialize . jData) . runGetL (many deserialize) $ d
 
-startArena :: forall m1 m2 m3 a b c
-           . (MonadIO m1, MonadIO m2, MonadIO m3, Serial a, Serial c, Semigroup b)
-           => (a -> b) -> (b -> c) -> (b -> Bool) -> ArenaLocation
-           -> m1 (m2 [(c, m2 [a])], a -> m3 ())
-startArena summarize finalize arenaFull diskLocation = liftIO $ do
-    currJournalM <- internArenas >>= cleanJournal >>= newMVar
-    dr           <- readAllData >>= newIORef
-    return (accessData dr currJournalM, addData dr currJournalM)
-  where
-    readAllData :: IO [(c, m2 [a])]
-    readAllData = do
-      ds <- Set.fromList . mapMaybe (readMay . dropExtension) <$>
-                       (getDirectoryContents . dataDir $ diskLocation)
-      forM (Set.toList ds) $ \d -> do
-        c <- runGetL deserialize <$> (BSL.readFile . dataSummaryFile diskLocation $ d)
-        return (c, readDataFl d)
-
-    accessData :: IORef [(c, m2 [a])] -> MVar (OpenJournal a b) -> m2 [(c, m2 [a])]
-    accessData dr cjM = liftIO $ do
-      withMVar cjM $ \(OJ _ _ s as) -> do
-        d <- readIORef dr
-        return $ (finalize . fromJust . getOption $ s, return . reverse $ as):d
-
-    addData :: IORef [(c, m2 [a])] -> MVar (OpenJournal a b) -> a -> m3 ()
-    addData dsr ojM a = liftIO . modifyMVar_ ojM $ \(OJ ai h s as) -> do
-     BS.hPutStr h . runPutS . serialize . mkJournal $ a
-     let s' = s <> (pure . summarize $ a)
-     syncHandle h -- This COULD be moved down for a very minor perf boost.
-     case arenaFull . fromJust . getOption $ s' of
-       False -> return $ OJ ai h s' (a:as)
-       True -> do
-         hClose h
-         internArenaFl ai
-         atomicModifyIORef' dsr $ \ods ->
-           ((finalize . fromJust . getOption $ s', readDataFl ai):ods, ())
-         let ai' = ai + 1
-         h' <- openFile (journalFile diskLocation $ ai') WriteMode
-         return $ OJ ai' h' mempty mempty
-
-    readDataFl :: MonadIO m => ArenaID -> m [a]
-    readDataFl = readDataFile diskLocation
-
-    internArenaFl :: ArenaID -> IO ()
-    internArenaFl ai = do
-      -- This has a problem if the journal was empty!
-      -- We never intern a journal until we've moved on to another though,
-      -- and to move on to another, we have to have had a summary to check for nursery fullness.
-      as <- readJournalFile diskLocation ai
-      withFileSync (dataFile diskLocation ai) $ \h ->
-        mapM_ (BS.hPutStr h . runPutS . serialize) as
-      withFileSync (dataSummaryFile diskLocation ai) $ \h ->
-        BS.hPutStr h . runPutS . serialize . finalize . alec $ as
-      removeFile . journalFile diskLocation $ ai
-
-    internArenas :: IO ArenaID
-    internArenas = do
-      (js::[ArenaID]) <- mapMaybe readMay <$> (getDirectoryContents . journalDir $ diskLocation)
-      case js of
-        [] -> return 0
-        _ -> do
-          let latest = maximum js
-          let old = delete latest js
-          mapM_ internArenaFl old
-          return latest
-
-    cleanJournal :: ArenaID -> IO (OpenJournal a b)
-    cleanJournal ai = do
-      te <- doesFileExist . tempJournal $ diskLocation
-      when te . removeFile . tempJournal $ diskLocation
-      je <- doesFileExist . journalFile diskLocation $ ai
-      case je of
-        False -> do
-         jh <- openFile (journalFile diskLocation ai) WriteMode
-         return $ OJ ai jh (Option Nothing) []
-        True -> do
-          as <- readJournalFile diskLocation $ ai
-          withFileSync (tempJournal diskLocation) $ \h ->
-            mapM_ (BS.hPutStr h . runPutS . serialize . mkJournal) as
-          renameFile (tempJournal diskLocation) (journalFile diskLocation ai)
-          jh <- openFile (journalFile diskLocation ai) WriteMode
-          return $ OJ ai jh (pure . alec $ as) (reverse as)
-
-    alec :: [a] -> b
-    alec = foldl1 (<>) . map summarize
-
 syncHandle :: Handle -> IO ()
 syncHandle h = do
   hFlush h
@@ -197,8 +101,25 @@ summarize d = asks acSummarize <*> pure d
 finalize :: Monad m => s -> ArenaT s f d m f
 finalize s = asks acFinalize <*> pure s
 
--- journalDir, dataDir, tempJournal :: ArenaLocation -> FilePath
--- journalFile, dataFile, dataSummaryFile :: ArenaLocation -> ArenaID -> FilePath
+journalDir, dataDir, tempJournal :: ArenaLocation -> FilePath
+journalDir (ArenaLocation fp) = fp            </> "journal"
+dataDir    (ArenaLocation fp) = fp            </> "data"
+tempJournal al                = journalDir al </> "temp"
+
+journalFile, dataFile, dataSummaryFile :: ArenaLocation -> ArenaID -> FilePath
+journalFile     al i = journalDir al </> show i
+dataFile        al i = dataDir    al </> addExtension (show i) "data"
+dataSummaryFile al i = dataDir    al </> addExtension (show i) "header"
+
+journalDir', dataDir', tempJournal' :: Monad m => ArenaT s f d m FilePath
+journalDir'  = asks (journalDir  . acArenaLocation)
+dataDir'     = asks (dataDir     . acArenaLocation)
+tempJournal' = asks (tempJournal . acArenaLocation)
+
+journalFile', dataFile', dataSummaryFile' :: Monad m => ArenaID -> ArenaT s f d m FilePath
+journalFile'     ai = asks (journalFile     . acArenaLocation) <*> pure ai
+dataFile'        ai = asks (dataFile        . acArenaLocation) <*> pure ai
+dataSummaryFile' ai = asks (dataSummaryFile . acArenaLocation) <*> pure ai
 
 data TheFiles = TheFiles {
       theJournalDir
@@ -207,7 +128,7 @@ data TheFiles = TheFiles {
     , theJournalFile
     , theDataFile
     , theDataSummaryFile :: FilePath
-} deriving (Eq, Ord, Read, Show)
+    } deriving (Eq, Ord, Read, Show)
 
 theFiles :: Monad m => Maybe ArenaID -> ArenaT s f d m TheFiles
 theFiles Nothing   = theFiles (Just $ -1)
@@ -237,24 +158,6 @@ runArena
   :: (Serial d, Serial s, Serial f, Semigroup s)
     => (d -> s) -> (s -> f) -> (s -> Bool) -> ArenaLocation -> Arena s f d a -> IO a
 runArena = runArenaT
-
-tempJournal' :: Monad m => ArenaT s f d m FilePath
-tempJournal' = asks (tempJournal . acArenaLocation)
-
-journalFile' :: Monad m => ArenaID -> ArenaT s f d m FilePath
-journalFile' ai = asks (journalFile . acArenaLocation) <*> pure ai
-
-journalDir' :: Monad m => ArenaT s f d m FilePath
-journalDir' = asks (journalDir . acArenaLocation)
-
-dataDir' :: Monad m => ArenaT s f d m FilePath
-dataDir' = asks (dataDir . acArenaLocation)
-
-dataFile' :: Monad m => ArenaID -> ArenaT s f d m FilePath
-dataFile' ai = asks (dataFile . acArenaLocation) <*> pure ai
-
-dataSummaryFile' :: Monad m => ArenaID -> ArenaT s f d m FilePath
-dataSummaryFile' ai = asks (dataSummaryFile . acArenaLocation) <*> pure ai
 
 readJournalFile' :: (Serial d, MonadIO m) => ArenaID -> ArenaT s f d m [d]
 readJournalFile' ai = do
@@ -321,12 +224,6 @@ readAllData = do
                 rdf <- readDataFile' d
                 return (c, rdf)
 
--- accessData :: IORef [(c, m2 [a])] -> MVar (OpenJournal a b) -> m2 [(c, m2 [a])]
--- accessData dr cjM = liftIO $ do
---   withMVar cjM $ \(OJ _ _ s as) -> do
---     d <- readIORef dr
---     return $ (finalize . fromJust . getOption $ s, return . reverse $ as):d
-
 accessData :: MonadIO m => ArenaT s f d m [(f, IO [d])]
 accessData = do
   ArenaConf {..} <- ask
@@ -352,20 +249,3 @@ addData d = do
                   let ai' = ai + 1
                   h' <- openFile (journalFile acArenaLocation ai') WriteMode
                   return $ OJ ai' h' mempty mempty
-
-
--- addData :: IORef [(c, m2 [a])] -> MVar (OpenJournal a b) -> a -> m3 ()
--- addData dsr ojM a = liftIO . modifyMVar_ ojM $ \(OJ ai h s as) -> do
---  BS.hPutStr h . runPutS . serialize . mkJournal $ a
---  let s' = s <> (pure . summarize $ a)
---  syncHandle h -- This COULD be moved down for a very minor perf boost.
---  case arenaFull . fromJust . getOption $ s' of
---    False -> return $ OJ ai h s' (a:as)
---    True -> do
---      hClose h
---      internArenaFl ai
---      atomicModifyIORef' dsr $ \ods ->
---        ((finalize . fromJust . getOption $ s', readDataFl ai):ods, ())
---      let ai' = ai + 1
---      h' <- openFile (journalFile diskLocation $ ai') WriteMode
---      return $ OJ ai' h' mempty mempty
--- tests/test.hs
+++ tests/test.hs
@@ -25,28 +25,6 @@ prepDirectory = do
   createDirectoryIfMissing True "test_data/journal"
   createDirectoryIfMissing True "test_data/data"
 
-
-main' :: IO ()
-main' = do
-  prepDirectory
-  (getState, addVal) <- startArena ((1::Sum Int,) . Sum) (getSum . snd) ((5 <) . getSum . fst) (ArenaLocation "test_data")
-  addVal (65::Int)
-  sm <- (sum . fmap fst) <$> getState
-  assert (sm == 65) "Count was wrong"
-  mapM_ addVal [(1::Int)..14]
-  sm <- (sum . fmap fst) <$> getState
-  as <- getState >>= mapM (\(c, act) -> (c,) <$> act)
-  putStrLn . show . sort $ as
-  assert (sm == (65 + sum [1..14])) "Larger sum incorrect!"
-  performMajorGC
-  threadDelay 100000
-  performMajorGC
-  (getState', _::Int -> IO ()) <- startArena ((1::Sum Int,) . Sum) (getSum . snd) ((5 <) . getSum . fst) (ArenaLocation "test_data")
-  as' <- getState' >>= mapM (\(c, act) -> (c,) <$> act)
-  putStrLn . show . sort $ as'
-  assert ((sort as) == (sort as')) "doesn't match old data"
-  return ()
-
 main :: IO ()
 main = do
   prepDirectory
